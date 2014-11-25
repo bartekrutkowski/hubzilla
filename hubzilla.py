e02bf454029fa240a3993f0c775a0e3d15455afc
Bartek Rutkowski <r@mba.local>
2014-11-25 19:04:48 +0000

    * Fixed russian debug print lime from previous commit

--- hubzilla.py
+++ hubzilla.py
@@ -59,9 +59,8 @@ def index():
                                  description='Diff file from pull request')
         print file_id
         print '{url}/?access_token={token}'.format(
-                url=pull_request['pull_request']['url'],
-                token=conf.get('github', 'token')),
-            data='{"status": "closed"}')
+            url=pull_request['pull_request']['url'],
+            token=conf.get('github', 'token'))
         close_pull_request = requests.patch(
             '{url}/?access_token={token}'.format(
                 url=pull_request['pull_request']['url'],
