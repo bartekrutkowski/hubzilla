79ea265cd2067c9f9ac651bbfb20e5384b11f44a
Bartek Rutkowski <r@mba.local>
2014-12-19 13:53:38 +0000

    * Initial version of @verify_request wrapper for GitHub requests authentication

--- hubzilla.py
+++ hubzilla.py
@@ -6,6 +6,7 @@ import requests
 
 import configparser
 from flask import Flask, request
+from functools import wraps
 
 app = Flask('__name__')
 app.secret_key = os.urandom(128)
@@ -42,41 +43,52 @@ def fill_problem_report(pull_request):
     return problem_report
 
 
+def verify_request(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            github_signature = request.headers.get('X-Hub-Signature')
+        except KeyError, e:
+            print "ERROR: Received request without X-Hub-Signature: {error}".format(error=e)
+            return False
+        print github_signature
+    return decorated_function
+
+
+@verify_request
 @app.route('/pull-request', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        pull_request = json.loads(request.data)
-        problem_report = fill_problem_report(pull_request)
-        bgz = bugzilla_connect()
-        problem_report = bgz.createbug(problem_report)
-        diff_file = io.StringIO(requests.get(
-                                pull_request['pull_request']['diff_url']).text)
-        file_id = bgz.attachfile(idlist=problem_report.id,
-                                 attachfile=diff_file,
-                                 name='pull_request.diff',
-                                 file_name='pull_request.diff',
-                                 is_patch=True,
-                                 description='Diff file from pull request')
-        comment = {"body": "This repository is a read only mirror of"
-            "official FreeBSD SVN repository. Your pull-request has been "
-            "transferred into FreeBSD bug tracker here:"
-            "https://bugs.freebsd.org/bugzilla/show_bug.cgi?"
-            "id={problem_report_id} where you can work with the FreeBSD "
-            "community on it."
-            "This pull request is closed automatically.".format(
-                problem_report_id=problem_report.id)}
-        comment_pull_request = requests.post(
-            '{url}?access_token={token}'.format(
-                url=pull_request['pull_request']['comments_url'],
-                token=conf.get('github', 'token')),
-            data=json.dumps(comment))
-        close_pull_request = requests.patch(
-            '{url}?access_token={token}'.format(
-                url=pull_request['pull_request']['url'],
-                token=conf.get('github', 'token')),
-            data='{"state": "closed"}')
-        print close_pull_request
-    return 'OK'
+    pull_request = json.loads(request.data)
+    problem_report = fill_problem_report(pull_request)
+    bgz = bugzilla_connect()
+    problem_report = bgz.createbug(problem_report)
+    diff_file = io.StringIO(requests.get(
+                            pull_request['pull_request']['diff_url']).text)
+    file_id = bgz.attachfile(idlist=problem_report.id,
+                             attachfile=diff_file,
+                             name='pull_request.diff',
+                             file_name='pull_request.diff',
+                             is_patch=True,
+                             description='Diff file from pull request')
+    comment = {"body": "This repository is a read only mirror of"
+        "official FreeBSD SVN repository. Your pull-request has been "
+        "transferred into FreeBSD bug tracker here:"
+        "https://bugs.freebsd.org/bugzilla/show_bug.cgi?"
+        "id={problem_report_id} where you can work with the FreeBSD "
+        "community on it."
+        "This pull request is closed automatically.".format(
+            problem_report_id=problem_report.id)}
+    comment_pull_request = requests.post(
+        '{url}?access_token={token}'.format(
+            url=pull_request['pull_request']['comments_url'],
+            token=conf.get('github', 'token')),
+        data=json.dumps(comment))
+    close_pull_request = requests.patch(
+        '{url}?access_token={token}'.format(
+            url=pull_request['pull_request']['url'],
+            token=conf.get('github', 'token')),
+        data='{"state": "closed"}')
+    print close_pull_request
 
 
 if __name__ == '__main__':
