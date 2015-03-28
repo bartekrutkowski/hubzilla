a8b0df1800443b2014b47792daa2cb84d617ce00
Bartek Rutkowski <contact@robakdesign.com>
2015-03-28 23:23:04 +0000

    First version of rewrite to Bugzilla REST API

--- hubzilla.py
+++ hubzilla.py
@@ -1,11 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import os
-import bugzilla
-import io
 import json
 import requests
 
 import configparser
-from flask import abort, Flask, request
+from flask import Flask, abort, make_response, request
 from functools import wraps
 from hmac import new
 from hashlib import sha1
@@ -17,36 +17,48 @@ conf = configparser.RawConfigParser()
 conf.read(os.environ['HUBZILLA_CONFIG'])
 
 
-def bugzilla_connect():
-    """Connect to Bugzilla instance and return callable object 'bgz'."""
+def to_unicode(text, encoding='utf-8'):
+    """Convert the text to unicode if it is not unicode already."""
 
-    bgz = bugzilla.Bugzilla(user=conf.get('bugzilla', 'user'),
-                            password=conf.get('bugzilla', 'password'),
-                            url=conf.get('bugzilla', 'url'),
-                            sslverify=False
-                            )
-    return bgz
+    if isinstance(text, basestring) and not isinstance(text, unicode):
+        return text.decode(encoding)
+    else:
+        return text
 
 
-def fill_problem_report(pull_request):
-    """Fills problem_report dict from pull_request data sent by GitHub hook."""
-    problem_report = {
-        'product': 'Ports Tree',
-        'component': 'Individual Port(s)',
-        'version': 'Latest',
-        'summary': 'GITHUB - IGNORE: Pull request #{number}: {title}'.format(
-            number=pull_request['pull_request']['number'],
-            title=pull_request['pull_request']['title']),
-        'description': '{description}\nBy: {name}({name_url})'.format(
-            description=pull_request['pull_request']['body'],
-            name=pull_request['pull_request']['user']['login'],
-            name_url=pull_request['pull_request']['user']['url']),
-        'url': '{url}'.format(url=pull_request['pull_request']['html_url'])
+def make_bug(pull_req):
+    """Makes problem_report dict from pull_request data sent by GitHub hook."""
+
+    bug = {
+        "product": "Ports & Packages",
+        "component": "Individual Port(s)",
+        "version": "Latest",
+        "summary": "GitHub Pull Request #{number}: {title}".format(
+            number=pull_req['pull_request']['number'],
+            title=pull_req['pull_request']['title']),
+        "description": "{description}\nBy: {name}({name_url})".format(
+            description=pull_req['pull_request']['body'],
+            name=pull_req['pull_request']['user']['login'],
+            name_url=pull_req['pull_request']['user']['url']),
+        "url": "{url}".format(url=pull_req['pull_request']['html_url'])
     }
-    return problem_report
+    return bug
 
 
-def verify_request(f):
+def make_patch(pr_id, data):
+    """Makes patch dictionary."""
+
+    patch = {
+        "ids": [pr_id],
+        "is_patch": "true",
+        "summary": "Patch from GitHub Pull Request",
+        "data": data,
+        "file_name": "pull_request.diff"
+    }
+    return patch
+
+
+def auth_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         try:
@@ -70,42 +82,52 @@ def verify_request(f):
 
 
 @app.route('/pull-request', methods=['POST'])
-@verify_request
+@auth_required
 def index():
-    pull_request = json.loads(request.data)
-    if pull_request['action'] == 'opened' and pull_request['pull_request']['state'] == 'open':
-        problem_report = fill_problem_report(pull_request)
-        bgz = bugzilla_connect()
-        problem_report = bgz.createbug(problem_report)
-        diff_file = io.StringIO(requests.get(
-                                pull_request['pull_request']['diff_url']).text)
-        file_id = bgz.attachfile(idlist=problem_report.id,
-                                 attachfile=diff_file,
-                                 name='pull_request.diff',
-                                 file_name='pull_request.diff',
-                                 is_patch=True,
-                                 description='Diff file from pull request')
+    try:
+        pull_req = json.loads(request.data)
+    except (ValueError, TypeError):
+        print "ERROR: pull-request data was not JSON."
+        abort(500)
+    if pull_req['action'] == 'opened' and pull_req['pull_request']['state'] == 'open':
+        problem_report = make_bug(pull_req)
+        post_url = '{url}/bug'.format(url=conf.get('bugzilla', 'url'))
+        post_params = {"api_key": "{key}".format(key=conf['bugzilla']['api_key'])}
+        try:
+            pr_id = requests.post(post_url, params=post_params, json=problem_report).json()['id']
+        except Exception as error:
+            print "ERROR: posting PR failed."
+            print error
+            abort(500)
+
+        post_url = '{url}/bug/{pr_id}/attachment'.format(
+                                                    url=conf.get('bugzilla', 'url'),
+                                                    pr_id=pr_id)
+        post_params = {"api_key": conf['bugzilla']['api_key']}
+        post_data = make_patch(pr_id, requests.get(pull_req['pull_request']['diff_url']).text)
+        file_id = requests.post(post_url, params=post_params, json=post_data)
+
         comment = {"body": "This repository is a read only mirror of "
-            "official FreeBSD SVN repository. Your pull-request has been "
-            "transferred into FreeBSD bug tracker here: "
-            "https://bugs.freebsd.org/bugzilla/show_bug.cgi?"
-            "id={problem_report_id} where you can work with the FreeBSD "
-            "community on resolving your Problem Report.\n\n"
-            "This pull request is closed automatically.".format(
-                problem_report_id=problem_report.id)}
+                   "official FreeBSD SVN repository. Your pull-request has "
+                   "been transferred into FreeBSD bug tracker here: "
+                   "https://bugs.freebsd.org/bugzilla/show_bug.cgi?"
+                   "id={pr_id} where you can work with the FreeBSD community "
+                   "on resolving your Problem Report.\n\n"
+                   "This pull request is closed automatically.".format(pr_id=pr_id)}
+
         comment_pull_request = requests.post(
             '{url}?access_token={token}'.format(
-                url=pull_request['pull_request']['comments_url'],
+                url=pull_req['pull_request']['comments_url'],
                 token=conf.get('github', 'token')),
             data=json.dumps(comment))
         close_pull_request = requests.patch(
             '{url}?access_token={token}'.format(
-                url=pull_request['pull_request']['url'],
+                url=pull_req['pull_request']['url'],
                 token=conf.get('github', 'token')),
             data='{"state": "closed"}')
-        print close_pull_request
+        return make_response('OK', 200)
     else:
-        print "ERROR: the pull-request is already already closed."
+        print "ERROR: the pull-request is already closed."
         abort(500)
 
 
